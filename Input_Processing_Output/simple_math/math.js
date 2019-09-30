134e3c318c4525761aea152f2d2bff8003052e82
Jenn <jenn.a.martinez@outlook.com>
2019-09-30 15:36:12 -0700

    adds inquirer prompt code

--- Input_Processing_Output/simple_math/math.js
+++ Input_Processing_Output/simple_math/math.js
@@ -1,1 +1,15 @@
-var inquirer = require('inquirer');
+var inquirer = requirer('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'first',
+            message: 'What is the first number?'
+        },
+        {
+            type: 'input',
+            name: 'second',
+            message: 'What is the second number?'
+        }
+    ])
